9bbbc30e25daa723ccbf7a45fcd21b478451e024
Raghulkannan005 <raghulkannan005@gmail.com>
2024-03-30 00:15:10 +0530

    fixed errors

--- Python/RockPaperScissor.py
+++ Python/RockPaperScissor.py
@@ -18,20 +18,20 @@ def rps():
         rps()
     if (bot==value):
         print("Game Draw!")
-    elif (bot="rock") and (value="paper" or value="scissor"):
+    elif (bot=="rock") and (value=="paper" or value=="scissor"):
         print("You Lose! Better luck next time")
-    elif (bot="paper") and (value="rock" or value="scissor"):
+    elif (bot=="paper") and (value=="rock" or value=="scissor"):
         print("You Won!")
-    elif (bot="scissor") and (value="paper" or value"rock"):
-        if (value = "rock"):
+    elif (bot=="scissor") and (value=="paper" or value=="rock"):
+        if (value == "rock"):
             print("You Won!")
         else:
             print("You Lose! Better luck next time")
     retry=int(input("1 -- Restart!\n2--Exit..\nEnter Your Choice:"))
-    if (retry=1):
+    if (retry==1):
         print("Restarting Game!")
         rps()
-    elif (retry=2):
+    elif (retry==2):
         print("Exiting...")
         quit()
 rps()
