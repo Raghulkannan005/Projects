efdcadc23f51e6037c2452b1062145dac6f87be6
Raghulkannan005 <raghulkannan005@gmail.com>
2024-04-08 00:13:08 +0530

    finished a raw rps game with bad design

--- JSProjects/RockPaperScissor/RPS.js
+++ JSProjects/RockPaperScissor/RPS.js
@@ -1,29 +1,37 @@
-playerchoice="rock"
-computerchoice="paper" // decisior tree//
-if(playerchoice===computerchoice){
-    console.log("Game Draw!");
-}
-else if(playerchoice==="paper"){
-    if(computerchoice==="scissor"){
-        console.log("Computer Wins!");
+const playerChoice = prompt("Choose rock, paper, or scissors:");
+
+const choices = ["rock", "paper", "scissors"];
+const index = Math.floor(Math.random() * choices.length);
+const computerChoice = choices[index];
+
+const playerDisplay = document.querySelector('.player');
+const computerDisplay = document.querySelector('.computer');
+const resultDisplay = document.querySelector('.result');
+
+playerDisplay.textContent = playerChoice;
+computerDisplay.textContent = computerChoice;
+
+const playerChoiceLower = playerChoice.toLowerCase();
+const computerChoiceLower = computerChoice.toLowerCase();
+
+if (playerChoiceLower === computerChoiceLower) {
+    resultDisplay.textContent = "Game Draw!";
+} else if (playerChoiceLower === "paper") {
+    if (computerChoiceLower === "scissors") {
+        resultDisplay.textContent = "Computer Wins!";
+    } else {
+        resultDisplay.textContent = "Player Wins!";
     }
-    else{
-        console.log("Player Wins!");
+} else if (playerChoiceLower === "rock") {
+    if (computerChoiceLower === "paper") {
+        resultDisplay.textContent = "Computer Wins!";
+    } else {
+        resultDisplay.textContent = "Player Wins!";
     }
-}
-else if(playerchoice==="rock"){
-    if(computerchoice==="paper"){
-        console.log("Computer Wins!");
-    }
-    else{
-        console.log("Player Wins!");
-    }
-}
-else if(playerchoice==="scissor"){
-    if(computerchoice==="rock"){
-        console.log("Computer Wins!");
-    }
-    else{
-        console.log("Player Wins!");
+} else if (playerChoiceLower === "scissors") {
+    if (computerChoiceLower === "rock") {
+        resultDisplay.textContent = "Computer Wins!";
+    } else {
+        resultDisplay.textContent = "Player Wins!";
     }
 }
