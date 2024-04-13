c6321806f9deb246ce826db6cecfe373069642f5
Raghulkannan005 <raghulkannan005@gmail.com>
2024-04-13 23:22:43 +0530

    Added FreeCodeCamp Practice Projects to in local and git

--- Learn/learn.js
+++
@@ -1,3 +0,0 @@
-a="raghulKannan"
-ran=(Math.floor(Math.random()*a.length))
-console.log(a.charAt(ran))
--- Learn/learn.py
+++
@@ -1,3 +0,0 @@
-# package = int(input("USD to INR\nEnter Package:"))
-# print(package*83.30,"Per Annum(in INR)\n",(package*83.30)/12,"Per month(in INR)")
-
