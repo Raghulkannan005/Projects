6dccc56794a686c31849f71ddaab3e7497b00149
Raghulkannan005 <raghulkannan005@gmail.com>
2024-04-15 17:35:33 +0530

    Rock Paper Scissor

--- Javascript Projects/RockPaperScissor/RPS.js
+++ Javascript Projects/RockPaperScissor/RPS.js
@@ -1,37 +1,42 @@
-const playerChoice = prompt("Choose rock, paper, or scissors:");
-
 const choices = ["rock", "paper", "scissors"];
+
+document.querySelector('button').addEventListener('click',function(){
+const playerChoice = document.querySelector('#userchoice').value.toLowerCase();
+if (!choices.includes(playerChoice)) {
+    alert("Error: Invalid choice");
+    return;
+}
 const index = Math.floor(Math.random() * choices.length);
-const computerChoice = choices[index];
+const computerChoice = choices[index].toLowerCase();
 
 const playerDisplay = document.querySelector('.player');
 const computerDisplay = document.querySelector('.computer');
 const resultDisplay = document.querySelector('.result');
-
-playerDisplay.textContent = playerChoice;
-computerDisplay.textContent = computerChoice;
-
-const playerChoiceLower = playerChoice.toLowerCase();
-const computerChoiceLower = computerChoice.toLowerCase();
-
-if (playerChoiceLower === computerChoiceLower) {
-    resultDisplay.textContent = "Game Draw!";
-} else if (playerChoiceLower === "paper") {
-    if (computerChoiceLower === "scissors") {
-        resultDisplay.textContent = "Computer Wins!";
-    } else {
-        resultDisplay.textContent = "Player Wins!";
-    }
-} else if (playerChoiceLower === "rock") {
-    if (computerChoiceLower === "paper") {
-        resultDisplay.textContent = "Computer Wins!";
-    } else {
-        resultDisplay.textContent = "Player Wins!";
-    }
-} else if (playerChoiceLower === "scissors") {
-    if (computerChoiceLower === "rock") {
-        resultDisplay.textContent = "Computer Wins!";
-    } else {
-        resultDisplay.textContent = "Player Wins!";
-    }
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+playerDisplay.textContent = capitalize(playerChoice);
+computerDisplay.textContent = capitalize(computerChoice);
+
+if (playerChoice === computerChoice) {
+    resultDisplay.textContent = "Game Draw!😒";
+} else if (playerChoice === "paper") {
+    if (computerChoice === "scissors") {
+        resultDisplay.textContent = `Computer Wins! 🤖`;
+    } else {
+        resultDisplay.textContent = `Player Wins!👑`;
+    }
+} else if (playerChoice === "rock") {
+    if (computerChoice === "paper") {
+        resultDisplay.textContent = `Computer Wins! 🤖`;
+    } else {
+        resultDisplay.textContent = `Player Wins!👑`;
+    }
+} else if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+        resultDisplay.textContent = `Computer Wins! 🤖`;
+    } else {
+        resultDisplay.textContent = `Player Wins!👑`;
+    }
+}});
