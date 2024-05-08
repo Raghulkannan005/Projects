bc242456f3ac1b292cecdf40078c04c242a72f02
Raghulkannan005 <raghulkannan005@gmail.com>
2024-05-08 20:36:53 +0530

    .

--- Javascript Projects/Mini Javascript (4-Projects)/Print Random Character/printrandom.js
+++
@@ -1,6 +0,0 @@
-function str() {
-    let word = document.getElementById("ip").value.trim();
-    let ind = Math.floor(Math.random()*(word.length));
-    document.getElementById("msg").innerHTML = word;
-    document.getElementById("smsg").innerHTML = (word.charAt(ind));
-}
